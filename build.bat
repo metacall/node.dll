4e284acf53de96718acddb93d813afcbfbd855db
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-12-15 00:02:35 +0200

    Initial commit.

---
+++ build.bat
@@ -0,0 +1,50 @@
+@echo on
+
+rem NodeJS Version
+set NODEJS_VERSION=%1
+
+rem Use all the cores when building
+set CL=/MP
+
+set loc=%cd%
+
+echo Checking Compiler and Build System
+
+rem Install NASM
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-win64.zip', './nasm.zip')" || goto :error
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "nasm.zip" -DestinationPath . || goto :error
+set PATH=%PATH%;%loc%\nasm-2.15.05;%loc%\nasm-2.15.05\rdoff
+del nasm.zip
+
+echo Downloading Dependencies
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://nodejs.org/download/release/v%NODEJS_VERSION%/node-v%NODEJS_VERSION%.tar.gz', './node_src.tar.gz')" || goto :error
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/metacall/core/66fcaac300611d1c4210023e7b260296586a42e0/cmake/NodeJSGYPPatch.py', './NodeJSGYPPatch.py')" || goto :error
+
+echo Building NodeJS as Shared Library
+
+rem Build NodeJS (DLL)
+cmake -E tar xzf node_src.tar.gz || goto :error
+cd %loc%\node-v%NODEJS_VERSION% || goto :error
+python %loc%\NodeJSGYPPatch.py %loc%\node-v%NODEJS_VERSION%\node.gyp || goto :error
+call .\vcbuild.bat dll
+mkdir %loc%\node-shared-v%NODEJS_VERSION%-x64 || goto :error
+move %loc%\node-v%NODEJS_VERSION%\out\Release\libnode.lib %loc%\node-shared-v%NODEJS_VERSION%-x64\libnode.lib || goto :error
+move %loc%\node-v%NODEJS_VERSION%\out\Release\libnode.dll %loc%\node-shared-v%NODEJS_VERSION%-x64\libnode.dll || goto :error
+
+echo NodeJS Built Successfully
+
+rem Delete unnecesary data
+rmdir /S /Q %loc%\node-v%NODEJS_VERSION%
+rmdir /S /Q %loc%\nasm-2.15.05
+
+echo Compressing the Tarball
+cd ../..
+powershell -Command "Compress-Archive" -Path %loc%\node-shared-v%NODEJS_VERSION%-x64 -DestinationPath node-shared-v%NODEJS_VERSION%-x64.zip
+
+exit 0
+
+rem Handle error
+:error
+echo Failed with error #%errorlevel%
+exit /b %errorlevel%
