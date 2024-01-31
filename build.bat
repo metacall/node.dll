80081425e669b1fbd2e20087b6b9d95182644b66
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2024-01-31 18:49:19 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -31,7 +31,7 @@ call .\vcbuild.bat dll
 
 echo NodeJS Built Successfully
 
-powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Compress-Archive" -Path %loc%\node-v%NODEJS_VERSION%\out\Release\libnode.lib, %loc%\node-v%NODEJS_VERSION%\out\Release\libnode.dll -DestinationPath %loc%\node-shared-v%NODEJS_VERSION%-x64.zip || goto :error
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Compress-Archive" -Path %loc%\node-v%NODEJS_VERSION%\out\Release\node.lib, %loc%\node-v%NODEJS_VERSION%\out\Release\node.dll -DestinationPath %loc%\node-shared-v%NODEJS_VERSION%-x64.zip || goto :error
 
 echo Tarball Compressed Successfully
 
